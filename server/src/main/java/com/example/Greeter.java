a311c4f09ba1c0f1a330872a0f0064ac6151e00a
fazil <fazil.hafiz@gmail.com>
2018-06-25 23:34:10 +0200

    on test un commit avec git2.18.0

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -5,15 +5,16 @@ package com.example;
  */
 public class Greeter {
 
-  /**
-   * This is a constructor.
-   */
-  public Greeter() {
+    /**
+     * This is a constructor.
+     */
+    public Greeter() {
 
-  }
+    }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
-    return String.format("Hello, %s!", someone);
-  }
+    //TODO: Add javadoc comment
+    public String greet(String someone) {
+        int b = 2 + 2;
+        return String.format("Hello, %s!", someone);
+    }
 }
